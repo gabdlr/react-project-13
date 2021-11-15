c672b466b6ab3030d93d4483123b4173f8d516ad
Gabriel De Los Rios <gab.delosrios@gmail.com>
2021-11-15 10:08:24 -0300

    Adressed some issues with errors handling

--- src/hooks/useAlertHandler.js
+++ src/hooks/useAlertHandler.js
@@ -2,19 +2,21 @@ import Swal from 'sweetalert2';
 
 function useAlertHandler (msg = "Success!", type) {
     //Watch this, TODO uniform server messages
-
+    console.log(msg)
     if(type === "error"){
         let errorList
         if(msg instanceof Object || Array.isArray(msg) ){
-            errorList = msg.join('</br>');
-            Swal.fire({
-                title: 'Error!',
-                html: errorList,
-                icon: 'error',
-            });
-            return;
-        } 
-        else {
+            if (msg.length > 0){
+                errorList = msg.join('</br>');
+                Swal.fire({
+                    title: 'Error!',
+                    html: errorList,
+                    icon: 'error',
+                });
+                return;
+            }
+        }
+        else if(msg.length === 0){
             Swal.fire({
                 title: 'Error!',
                 text: msg.msg,
@@ -22,6 +24,15 @@ function useAlertHandler (msg = "Success!", type) {
             });
             return;
         }
+        else{
+            Swal.fire({
+                title: 'Error!',
+                text: "Something went wrong",
+                icon: 'error'
+            });
+            return;  
+        } 
+
     }
     Swal.fire({
         title: 'Success!',
